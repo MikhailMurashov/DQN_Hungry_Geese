e4acc18c0ffa5cafcbf8c7814660cf1b5ac03364
m.murashov <m.murashov@muzlab.ru>
2021-03-25 00:14:51 +0300

    prepare config

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
-min_frames_for_train = 100
+min_frames_for_train = 2
 buffer_size = 50000
-num_iterations = 100000
-atari_shape = (4, 105, 80)
+num_iterations = 100
+save_target_model = 500
 
-num_frames = 4
+num_frames = 2
 num_actions = 4
+geese_shape = (num_frames, 11, 7)
 
-batch_size = 32
+batch_size = 1
 learning_rate = 0.00025
 
 initial_epsilon = 1.0
 final_epsilon = 0.05
-final_iteration_num = 10000
+final_iteration_num = num_iterations / 5
 
 gamma = 0.99  # decay rate of past observations
