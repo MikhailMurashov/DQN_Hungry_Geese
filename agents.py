7800e16a491d50862b815142f89e2857b3919b56
m.murashov <m.murashov@muzlab.ru>
2021-03-24 22:36:40 +0300

    fix default_agent

--- agents.py
+++ agents.py
@@ -5,18 +5,19 @@ def smart_agent(obs_dict, config_dict):
     pass
 
 
-def default_agent(obs_dict, config_dict):
+def default_agent(obs_dict, config_dict=None):
     """This agent always moves toward observation.food[0] but does not take advantage of board wrapping"""
     observation = Observation(obs_dict)
-    configuration = Configuration(config_dict)
+    # configuration = Configuration(config_dict)
+    configuration_columns = 11
 
     player_index = observation.index
     player_goose = observation.geese[player_index]
     player_head = player_goose[0]
-    player_row, player_column = row_col(player_head, configuration.columns)
+    player_row, player_column = row_col(player_head, configuration_columns)
 
     food = observation.food[0]
-    food_row, food_column = row_col(food, configuration.columns)
+    food_row, food_column = row_col(food, configuration_columns)
 
     if food_row > player_row:
         return Action.SOUTH.name
